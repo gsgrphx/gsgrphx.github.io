6769f76ea7478aa34cde23240d79ed699fa655f5
gsgrphx <65813111+gsgrphx@users.noreply.github.com>
2020-11-19 15:25:04 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ $(document).ready(function(){
 			$('body,html').animate({scrollTop: top}, 500);
 	});
 });
+
+$(document).ready(function(){
+	$(".front__second-line").on("click","a", function (event) {
+		event.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 500);
+	});
+});
+
+
+// = = бургер = = =
+$(document).ready(function() {
+	$('.front__burger').click(function(event) {
+		$('.front__burger, .front__topmenu, .front__center').toggleClass('active');
+	});
+});
+// = = бургер = = =
+
+$(document).ready(function() {
+	$('.inTop').click(function(event) {
+		$('.front__burger, .front__topmenu, .front__center').removeClass('active');
+	});
+});
